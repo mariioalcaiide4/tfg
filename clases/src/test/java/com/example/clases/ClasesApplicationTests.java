96c25e28581e6e471e3943627d6aa2122282fc62
Mario Alcaide Pérez <mario.alcaideperez@iesvalleinclan.es>
2025-11-11 10:47:08 +0100

    Microservicios

---
+++ clases/src/test/java/com/example/clases/ClasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
